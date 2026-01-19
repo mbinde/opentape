14e0b67681cefe596e1ba858337fb16c6ba2e768
mbinde <m@binde.dev>
2026-01-19 13:36:57 -0800

    Update index.php and warning.php for userdata structure and PHP 8
    - Use get_setup_status() instead of checking old paths directly
    - Update PHP version requirement from 5.0 to 8.0
    - Show detailed setup instructions for control panel users
    - Modern HTML5 doctype and markup

--- code/warning.php
+++ code/warning.php
@@ -1,44 +1,73 @@
 <?php
+require_once("opentape_common.php");
 
-	include("opentape_common.php");
-	
-?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-	<head>
-		<title>Warning - Fix issue before proceeding! / Opentape</title>
-		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-		<link rel="stylesheet" type="text/css" href="<?php echo $REL_PATH; ?>res/style.css" />
-	</head>
-	<body>
-		<div class="container">
-            <div class="banner">
-				<h1>OPENTAPE</h1>
-			    <div class="ajax_status"></div>
-			</div>	
+$setup_status = get_setup_status();
+$php_version_ok = version_compare(PHP_VERSION, '8.0.0', '>=');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Setup Required / Opentape</title>
+    <link rel="stylesheet" href="<?php echo $REL_PATH; ?>res/style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="banner">
+            <h1>OPENTAPE</h1>
+            <div class="ajax_status"></div>
+        </div>
 
-			<div class="content">
-            <div class="section">
-			<h2 style="color:#f00;">Uh oh!</h2>
+        <div class="content">
+            <div class="section" style="background:#fee; border:1px solid #c00; padding:15px;">
+                <h2 style="color:#c00;">Setup Required</h2>
 
-			<?php 
-				if ( !is_writeable('settings') || !is_writeable('songs') ) {
-			?>
-			<p>Looks like two directories that need to be writeable, aren't. :(</p>
-			<p>Please make <span style="color: #f00;">/songs/</span> and <span style="color: #f00;">/settings/</span> writeable by the web server user.  Your webhost should be able to provide more information on how to quickly accomplish this.</p>			
-			<p>Then refresh this page and you'll be all set.</p>
-			<?php
-				} elseif ( phpversion() < '5.0.0' ) {
-			?>
-			<p>Looks like you are running PHP version <?php echo phpversion(); ?>.  Opentape currently requires version 5+ to be installed on your webserver.</p>
-			<?php
-				}
-			?>
-			</div>
-		<div class="footer">
-			<?php get_version_banner(); ?>
-		</div>		
-		</div>
-	</div>
-		
-	</body>
+<?php if (!$php_version_ok): ?>
+                <p>Opentape requires <strong>PHP 8.0 or higher</strong>. You are running PHP <?php echo htmlspecialchars(PHP_VERSION); ?>.</p>
+                <p>Please contact your web host to upgrade PHP.</p>
+
+<?php elseif (!$setup_status['settings_writable'] || !$setup_status['songs_writable']): ?>
+                <p>Opentape needs to create some directories to store your music and settings, but doesn't have permission.</p>
+
+                <p><strong>What needs to be fixed:</strong></p>
+                <ul style="margin:10px 0; padding-left:20px;">
+<?php if (!$setup_status['userdata_dir']): ?>
+                    <li>Create the <code>userdata/</code> directory</li>
+<?php elseif (!$setup_status['userdata_writable']): ?>
+                    <li>Make <code>userdata/</code> writable (chmod 755 or 775)</li>
+<?php endif; ?>
+<?php if (!$setup_status['settings_dir']): ?>
+                    <li>Create the <code>userdata/settings/</code> directory</li>
+<?php elseif (!$setup_status['settings_writable']): ?>
+                    <li>Make <code>userdata/settings/</code> writable (chmod 755 or 775)</li>
+<?php endif; ?>
+<?php if (!$setup_status['songs_dir']): ?>
+                    <li>Create the <code>userdata/songs/</code> directory</li>
+<?php elseif (!$setup_status['songs_writable']): ?>
+                    <li>Make <code>userdata/songs/</code> writable (chmod 755 or 775)</li>
+<?php endif; ?>
+                </ul>
+
+                <p><strong>How to fix (using your hosting control panel):</strong></p>
+                <ol style="margin:10px 0; padding-left:20px; line-height:1.6;">
+                    <li>Open your hosting control panel (cPanel, Plesk, etc.)</li>
+                    <li>Go to <strong>File Manager</strong></li>
+                    <li>Navigate to your Opentape folder</li>
+                    <li>Create the <code>userdata</code> folder if it doesn't exist</li>
+                    <li>Right-click on it and select <strong>Permissions</strong></li>
+                    <li>Set permissions to <strong>755</strong> (or 775 if 755 doesn't work)</li>
+                    <li>Create <code>settings</code> and <code>songs</code> folders inside <code>userdata</code></li>
+                    <li>Set the same permissions on those folders</li>
+                </ol>
+
+                <p><a href="<?php echo $REL_PATH; ?>">Refresh this page</a> after making changes.</p>
+<?php endif; ?>
+            </div>
+
+            <div class="footer">
+                <?php get_version_banner(); ?>
+            </div>
+        </div>
+    </div>
+</body>
 </html>
--- index.php
+++ index.php
@@ -1,26 +1,29 @@
 <?php
-	
-	// Welcome to Opentape!
-	// This file is kept intentionally simple.  If you are seeing it in your web browser, you probably do not have PHP installed :(
-	
-	set_include_path(get_include_path() . PATH_SEPARATOR . "./code/");
+/**
+ * Opentape - Entry Point
+ */
 
-	// check if critical directories are writeable
-	if ( !is_writeable('settings') || !is_writeable('songs') ) {
-		include ("code/warning.php");
-	
-	// Current version designed for PHP5+, though we may update to support 4.X.X
-	} elseif (phpversion() < '5.0.0') {
-		include ("code/warning.php");
-	
-	// If no password is set, show password creation screen
-	} elseif ( file_exists ('settings/.opentape_password.php') === false || filesize ('settings/.opentape_password.php') == 0) {
-		include ("code/welcome.php");
-	
-	// otherwise, all is well!
-	} else {
-		include("code/mixtape.php");	
-	
-	}
+set_include_path(get_include_path() . PATH_SEPARATOR . "./code/");
+require_once("opentape_common.php");
 
-?>
+// Check PHP version
+if (version_compare(PHP_VERSION, '8.0.0', '<')) {
+    include("code/warning.php");
+    exit;
+}
+
+// Check if userdata directories are set up and writable
+$setup_status = get_setup_status();
+if (!$setup_status['settings_writable'] || !$setup_status['songs_writable']) {
+    include("code/warning.php");
+    exit;
+}
+
+// If no password is set, show password creation screen
+if (!is_password_set()) {
+    include("code/welcome.php");
+    exit;
+}
+
+// All good - show the mixtape
+include("code/mixtape.php");
